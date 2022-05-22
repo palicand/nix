8c55f80a030352d3f54fcdf256e21e6a5e4ceca0
Andrej Palicka <andrej.palicka@gmail.com>
2022-05-22 19:57:09 +0200

    Git config

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -29,7 +29,7 @@
 
   homebrew = {
     enable = true;
-    autoUpdate = true;
+    autoUpdate = false;
     global = {
       brewfile = true;
       noLock = true;
@@ -102,37 +102,93 @@
       kubernetes-helm
       cachix
       man-db
-      git
+      nix-doc
     ];
-    programs.zsh = {
-      localVariables = {
-        LANG = "en_US.UTF-8";
-        GPG_TTY = "/dev/ttys000";
-        CLICOLOR = 1;
-        LS_COLORS = "ExFxBxDxCxegedabagacad";
-        TERM = "xterm-256color";
+    programs = {
+      zsh = {
+        localVariables = {
+          LANG = "en_US.UTF-8";
+          GPG_TTY = "/dev/ttys000";
+          CLICOLOR = 1;
+          LS_COLORS = "ExFxBxDxCxegedabagacad";
+          TERM = "xterm-256color";
+        };
+
+        enable = true;
+        autocd = true;
+        oh-my-zsh = {
+          enable = true;
+          plugins = [ "git" "sudo" "common-aliases" ];
+        };
+        initExtra = ''
+          if [[ -d /opt/homebrew ]]; then
+                  eval "$(/opt/homebrew/bin/brew shellenv)"
+          fi
+          SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+        '';
+        shellAliases = {
+          grep = "rg";
+          cat = "bat";
+          iftop = "bandwhich";
+          ua = "sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y";
+          whatismyip = "dig +short myip.opendns.com @resolver1.opendns.com";
+        };
       };
 
-      enable = true;
-      autocd = true;
-      oh-my-zsh = {
+      git = {
         enable = true;
-        plugins = [ "git" "sudo" "common-aliases" ];
+        userName = "Andrej Palicka";
+        userEmail = "andrej.palicka@gmail.com";
+        aliases = {
+          lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+          fap = "fetch -ap";
+          co = "checkout";
+          cob = "checkout -b";
+          unstage = "reset HEAD --";
+          ci = "commit";
+          ciam = "commit -am";
+          st = "status";
+          br = "branch";
+          type = "cat-file -t";
+          dump = "cat-file -p";
+          ff = "merge --ff";
+        };
+        signing = {
+          key = "7E2DD79792CEC919";
+          signByDefault = true;
+        };
+        ignores = [
+          ".vscode"
+          ".mypy_cache"
+          ".pytest_cache"
+          "docker-compose.dev.yaml"
+          ".env"
+          "docs/README.md"
+          "*.~lock*"
+          "*.egg-info/"
+          ".idea/"
+          ".metals/"
+          ".bloop/"
+          "target/"
+        ];
+        extraConfig = {
+          rerere = {
+            enabled = true;
+          };
+        };
+        includes = [{
+            contents = {
+              core = {
+	              editor = "vim";
+              };
+              push = {
+                default = "simple";
+              };
+            };
+          }
+        ];
       };
-      initExtra = ''
-        if [[ -d /opt/homebrew ]]; then
-                eval "$(/opt/homebrew/bin/brew shellenv)"
-        fi
-      '';
-      shellAliases = {
-        grep = "rg";
-        cat = "bat";
-        iftop = "bandwhich";
-        ua = "sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y";
-        whatismyip = "dig +short myip.opendns.com @resolver1.opendns.com";
-      };
-    };
-    programs = {
+
       starship = {
         enable = true;
         package = pkgs.starship;
@@ -149,6 +205,10 @@
         enable = true;
         viAlias = true;
         vimAlias = true;
+      };
+
+      gitui = {
+        enable = true;
       };
     };
 
